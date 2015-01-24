9a0d5e38406eb522c8d4bddcb6446e8267b2007f
IjzerenHein <hrutjes@gmail.com>
2015-01-24 20:52:40 +0100

    Work in progress

--- src/main.js
+++ src/main.js
@@ -26,11 +26,8 @@ define(function(require) {
 
     // import dependencies
     var Engine = require('famous/core/Engine');
-    var RenderNode = require('famous/core/RenderNode');
-    var Modifier = require('famous/core/Modifier');
     var Surface = require('famous/core/Surface');
     var LayoutController = require('famous-flex/LayoutController');
-    var HeaderFooterLayout = require('famous-flex/layouts/HeaderFooterLayout');
     var FlexScrollView = require('famous-flex/FlexScrollView');
     var TabBar = require('famous-flex/widgets/TabBar');
 
@@ -38,95 +35,75 @@ define(function(require) {
     var mainContext = Engine.createContext();
 
     // create main layout
-    var layout = _createLayout();
-    mainContext.add(layout);
+    var scrollView = _createScrollView();
+    mainContext.add(scrollView);
 
     //
-    // Header-footer layout
+    // Add examples
     //
-    function _createLayout() {
-        var tabBarSize = [undefined, 50];
+    _createTabBar('Regular', {
+        tabBarLayout: {
+            itemSize: undefined
+        },
+        classes: ['tabbar-white']
+    });
+    _createTabBar('itemSize: 80', {
+        tabBarLayout: {
+            itemSize: 100
+        },
+        classes: ['tabbar-white']
+    });
+    _createTabBar('itemSize: true', {
+        size: [true, undefined],
+        tabBarLayout: {
+            itemSize: true,
+            margins: [0, 10],
+            spacing: 10
+        },
+        classes: ['tabbar-black']
+    });
+    _createTabBar('slow-motion', {
+        tabBarLayout: {
+            itemSize: undefined
+        },
+        layoutController: {
+            nodeSpring: {
+                dampingRatio: 0.8,
+                period: 1000
+            }
+        },
+        classes: ['tabbar-black']
+    });
+    _createTabBar('bouncy', {
+        size: [true, undefined],
+        tabBarLayout: {
+            itemSize: true,
+            margins: [0, 20],
+            spacing: 20
+        },
+        layoutController: {
+            nodeSpring: {
+                dampingRatio: 0.4,
+                period: 400
+            }
+        },
+        classes: ['tabbar-blue']
+    });
+
+    //
+    // Main scrollview
+    //
+    function _createScrollView() {
         return new FlexScrollView({
             mouseMove: true,
             autoPipeEvents: true,
-            dataSource: [
-                _createHeader('itemSize: undefined'),
-                _createTabBar({
-                    size: tabBarSize,
-                    tabBarLayout: {
-                        itemSize: undefined
-                    }
-                }),
-                _createSpacer(),
-
-                _createHeader('itemSize: 100'),
-                _createTabBar({
-                    size: tabBarSize,
-                    tabBarLayout: {
-                        itemSize: 100
-                    }
-                }),
-                _createSpacer(),
-
-                _createHeader('itemSize: true'),
-                _createTabBar({
-                    size: tabBarSize,
-                    tabBarLayout: {
-                        itemSize: true,
-                        margins: [0, 10],
-                        spacing: 10
-                    }
-                }),
-                _createSpacer(),
-
-                _createHeader('slow motion'),
-                _createTabBar({
-                    size: tabBarSize,
-                    tabBarLayout: {
-                        itemSize: undefined
-                    },
-                    layoutController: {
-                        nodeSpring: {
-                            dampingRatio: 0.8,
-                            period: 1000
-                        }
-                    }
-                }),
-                _createSpacer(),
-
-                _createHeader('bouncy'),
-                _createTabBar({
-                    size: tabBarSize,
-                    tabBarLayout: {
-                        itemSize: true,
-                        margins: [0, 20],
-                        spacing: 20
-                    },
-                    layoutController: {
-                        nodeSpring: {
-                            dampingRatio: 0.4,
-                            period: 400
-                        }
-                    }
-                }),
-                _createSpacer()
-            ]
+            layoutOptions: {
+                margins: [10, 20],
+                itemSize: 100
+            }
         });
     }
 
-    function _createSpacer() {
-        return new RenderNode(new Modifier({
-            size: [undefined, 20]
-        }));
-    }
-
-    function _createHeader(text) {
-        return new Surface({
-            size: [undefined, 30],
-            classes: ['header'],
-            content: text
-        });
-    }
     function _createTabItem(text, itemSize) {
         return new Surface({
             classes: ['tabbar-item'],
@@ -135,18 +112,39 @@ define(function(require) {
         });
     }
 
-    function _createTabBar(options) {
+    function _createTabBar(title, options) {
         var tabBar = new TabBar(options);
         tabBar.setItems([
             _createTabItem('one', options.tabBarLayout.itemSize),
             _createTabItem('fourty', options.tabBarLayout.itemSize),
             _createTabItem('lorum ipsum', options.tabBarLayout.itemSize)
         ]);
+        tabBar.setBackground(
+            new Surface({
+                classes: ['tabbar-background']
+            })
+        );
         tabBar.setSelectedItemOverlay(
             new Surface({
                 classes: ['tabbar-selected-item']
             })
         );
-        return tabBar;
+        scrollView.push(new LayoutController({
+            layout: {dock: [
+                ['top', 'header', 30],
+                ['bottom', 'footer', 20],
+                ['left', 'content']
+            ]},
+            dataSource: {
+                header: new Surface({
+                    classes: ['header'],
+                    content: title
+                }),
+                footer: new Surface({
+                    classes: ['footer']
+                }),
+                content: tabBar
+            }
+        }));
     }
 });
