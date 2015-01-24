72aeed67e51a92cec0394a0f0cfb65ddf3e42a41
IjzerenHein <hrutjes@gmail.com>
2015-01-24 14:15:29 +0100

    Preliminary checkin

--- src/main.js
+++ src/main.js
@@ -22,9 +22,12 @@ define(function(require) {
 
     // import dependencies
     var Engine = require('famous/core/Engine');
+    var RenderNode = require('famous/core/RenderNode');
+    var Modifier = require('famous/core/Modifier');
     var Surface = require('famous/core/Surface');
     var LayoutController = require('famous-flex/LayoutController');
     var HeaderFooterLayout = require('famous-flex/layouts/HeaderFooterLayout');
+    var FlexScrollView = require('famous-flex/FlexScrollView');
     var TabBar = require('famous-flex/widgets/TabBar');
 
     // create the main context
@@ -38,32 +41,102 @@ define(function(require) {
     // Header-footer layout
     //
     function _createLayout() {
-        return new LayoutController({
-            layout: HeaderFooterLayout,
-            layoutOptions: {
-                headerSize: 60
-            },
-            dataSource: {
-                header: _createTabBar(),
-                content: new Surface()
-            }
+        var tabBarSize = [undefined, 50];
+        return new FlexScrollView({
+            mouseMove: true,
+            autoPipeEvents: true,
+            dataSource: [
+                _createHeader('itemSize: undefined'),
+                _createTabBar({
+                    size: tabBarSize,
+                    tabBarLayout: {
+                        itemSize: undefined
+                    }
+                }),
+                _createSpacer(),
+
+                _createHeader('itemSize: 100'),
+                _createTabBar({
+                    size: tabBarSize,
+                    tabBarLayout: {
+                        itemSize: 100
+                    }
+                }),
+                _createSpacer(),
+
+                _createHeader('itemSize: true'),
+                _createTabBar({
+                    size: tabBarSize,
+                    tabBarLayout: {
+                        itemSize: true,
+                        margins: [0, 10],
+                        spacing: 10
+                    }
+                }),
+                _createSpacer(),
+
+                _createHeader('slow motion'),
+                _createTabBar({
+                    size: tabBarSize,
+                    tabBarLayout: {
+                        itemSize: undefined
+                    },
+                    layoutController: {
+                        nodeSpring: {
+                            dampingRatio: 0.8,
+                            period: 1000
+                        }
+                    }
+                }),
+                _createSpacer(),
+
+                _createHeader('bouncy'),
+                _createTabBar({
+                    size: tabBarSize,
+                    tabBarLayout: {
+                        itemSize: true,
+                        margins: [0, 20],
+                        spacing: 20
+                    },
+                    layoutController: {
+                        nodeSpring: {
+                            dampingRatio: 0.4,
+                            period: 400
+                        }
+                    }
+                }),
+                _createSpacer()
+            ]
         });
     }
 
-    function _createTabItem(text, image) {
+    function _createSpacer() {
+        return new RenderNode(new Modifier({
+            size: [undefined, 20]
+        }));
+    }
+
+    function _createHeader(text) {
+        return new Surface({
+            size: [undefined, 30],
+            classes: ['header'],
+            content: text
+        });
+    }
+    function _createTabItem(text, itemSize) {
         return new Surface({
             classes: ['tabbar-item'],
+            size: (itemSize === true) ? [true, undefined] : undefined,
             content: text
         });
     }
 
-    function _createTabBar() {
-        var tabBar = new TabBar({
-        });
+    function _createTabBar(options) {
+        var tabBar = new TabBar(options);
         tabBar.setItems([
-            _createTabItem('one'),
-            _createTabItem('two'),
-            _createTabItem('three')
+            _createTabItem('one', options.tabBarLayout.itemSize),
+            _createTabItem('fourty', options.tabBarLayout.itemSize),
+            _createTabItem('lorum ipsum', options.tabBarLayout.itemSize)
         ]);
         tabBar.setSelectedItemOverlay(
             new Surface({
